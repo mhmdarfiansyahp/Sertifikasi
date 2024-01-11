7d2aa825089e4905a3804310c6756414df3ab68f
mhmdarfiansyahp <mhmdarfiansyahp@gmail.com>
2024-01-12 01:17:32 +0700

    dan jadi semua

---
+++ Sertifikasi/Controllers/DetailSertifikasiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sertifikasi.Controllers
+{
+    public class DetailSertifikasiController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
